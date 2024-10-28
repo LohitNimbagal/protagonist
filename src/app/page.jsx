416d39bd4f60a4791d4466e592036a8b41572464
LohitNimbagal <nimbagallohit@gmail.com>
2024-10-28 19:16:33 +0530

    update  stats

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -137,17 +137,17 @@ export default function NotionTemplateLanding() {
               </div>
               <div className="text-center">
                 <Users className="w-8 h-8 md:w-12 md:h-12 mx-auto mb-4 text-[#253d30]" />
-                <p className="text-2xl font-bold text-[#253d30] ">176K+</p>
+                <p className="text-2xl font-bold text-[#253d30] ">177K+</p>
                 <p className="text-gray-900">YouTube Subscribers</p>
               </div>
               <div className="text-center">
                 <VideoIcon className="w-8 h-8 md:w-12 md:h-12 mx-auto mb-4 text-[#253d30]" />
-                <p className="text-2xl font-bold text-[#253d30] ">130+</p>
+                <p className="text-2xl font-bold text-[#253d30] ">200+</p>
                 <p className="text-gray-900">YouTube Videos</p>
               </div>
               <div className="text-center">
                 <Eye className="w-8 h-8 md:w-12 md:h-12 mx-auto mb-4 text-[#253d30]" />
-                <p className="text-2xl font-bold text-[#253d30] ">10M+</p>
+                <p className="text-2xl font-bold text-[#253d30] ">19M</p>
                 <p className="text-gray-900">Total Views</p>
               </div>
             </div>
